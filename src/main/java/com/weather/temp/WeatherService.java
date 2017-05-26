32b90f88ff19175ea807f0e8a1e4df4a0f0cb8ba
Keshav Kumar <keshav.k987@gmail.com>
2017-05-26 17:40:35 +0530

    Update WeatherService.java

--- src/main/java/com/weather/temp/WeatherService.java
+++ src/main/java/com/weather/temp/WeatherService.java
@@ -10,12 +10,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class WeatherService {
 
-	@Autowired
-	private WeatherRepository weatherRepository;
+     @Autowired
+     private WeatherRepository weatherRepository;
 	
-    public List<Temperature> getAllTemperature(){
+    public List<Temperature> getAllTemperature() throws ParseException{
     	List<Temperature> weather=new ArrayList<>();
     	weatherRepository.findAll().forEach(weather::add);
+    	DateFormat formatter = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+    	Date now=new Date();
+    	String sDate=formatter.format(now);
+    	now=formatter.parse(sDate);
+    	Iterator itr=weather.listIterator();
+    	while(itr.hasNext()){
+    		Temperature t=(Temperature)itr.next();
+    		Date d=formatter.parse( t.getDate() );
+    		if(now.compareTo(d)>0){
+    			itr.remove();
+    		}
+    	}	
     	return weather;
     }
     
